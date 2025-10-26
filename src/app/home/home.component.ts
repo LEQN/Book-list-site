385e89e728c88c3261bddebb4504a23fe0ec1a54
LEQN <leonmorin410@gmail.com>
2025-10-26 22:19:27 +0000

    [CREATED] home page component.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bib-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
