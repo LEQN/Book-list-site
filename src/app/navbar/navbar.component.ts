16184f3803e83a196331444258ec788dff886869
LEQN <leonmorin410@gmail.com>
2025-10-26 17:34:58 +0000

    [CREATED] navbar component
    added navbar component with styling and logo for site.

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bib-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
