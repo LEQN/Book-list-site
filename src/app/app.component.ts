b67758fbbb92418af29810ab91f46077a1e98935
LEQN <leonmorin410@gmail.com>
2025-10-31 23:54:40 +0000

    [Created] search page component.
    Basic html & css for search page. Also added some styling to app
    component to utilise 100vh and push footer down to bottom.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from "./home/home.component";
+import { SearchComponent } from "./search/search.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
