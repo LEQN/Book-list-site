32d2500f8850425fedd396194a303303e220d38b
LEQN <leonmorin410@gmail.com>
2025-11-01 16:23:17 +0000

    [Added] navbar routing.
    Routing for 3 pages, redirect for empty url.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
-import { HomeComponent } from "./home/home.component";
-import { SearchComponent } from "./search/search.component";
-import { ListComponent } from "./list/list.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, SearchComponent, ListComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { ListComponent } from './list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component:HomeComponent, title:'Home'},
+    {path: 'search', component:SearchComponent, title:'Search'},
+    {path: 'list', component:ListComponent, title:'My List'},
+    {path: '', redirectTo:'/home', pathMatch:'full'}
+];
