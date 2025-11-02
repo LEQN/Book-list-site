81da39e9567199150e070b41750a7602ce9bf9ea
LEQN <leonmorin410@gmail.com>
2025-11-02 23:33:17 +0000

    [Added] Form valdiation and input.
    Form submission gets input search query and contains form validation requiring input.
    Displays error div if invalid input.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), 
     provideClientHydration(withEventReplay()),
-    provideHttpClient()
+    provideHttpClient(),
+    importProvidersFrom(FormsModule)
   ]
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { CarouselComponent } from "./carousel/carousel.component";
 import { Books } from '../books';
 import { LibraryDataService } from '../services/library-data.service';
 
 @Component({
   selector: 'bib-home',
-  imports: [CarouselComponent],
+  imports: [CarouselComponent, FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -13,6 +15,8 @@ export class HomeComponent {
   trendingBooks: Books[] = [];
   classicBooks: Books[] = [];
   fictionBooks: Books[] = [];
+
+  searchQuery: string = "";
 
   constructor(private librarySvc:LibraryDataService){}
 
@@ -32,5 +36,9 @@ export class HomeComponent {
 
   getFiction(){
     this.librarySvc.getFictionBooks().subscribe(books => this.fictionBooks = books);
+  }
+
+  onSubmit(){
+    console.log(`Search submitted... ${this.searchQuery}`);
   }
 }
